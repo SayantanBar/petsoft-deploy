25d4e092e0f76bc0e0b73efad2add8a4b57f3f37
SayantanBar <121619214+SayantanBar@users.noreply.github.com>
2024-11-05 17:39:42 +0530

    Update route.ts

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,23 +1,25 @@
 import prisma from "@/lib/db";
+
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request: Request) {
   const body = await request.text();
   const signature = request.headers.get("stripe-signature");
-  //verify webhook came from stripe
+
+  // verify webhook came from Stripe
   let event;
   try {
-    event = stripe.webhook.constructEvent(
+    event = stripe.webhooks.constructEvent(
       body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
     );
-  } catch (err) {
-    console.log("webhook verification failed", err);
-    return Response.json(null, { status: 404 });
+  } catch (error) {
+    console.log("Webhook verification failed", error);
+    return Response.json(null, { status: 400 });
   }
 
-  //fulfill order
+  // fulfill order
   switch (event.type) {
     case "checkout.session.completed":
       await prisma.user.update({
@@ -30,10 +32,9 @@ export async function POST(request: Request) {
       });
       break;
     default:
-      console.log(`Unhandle event type${event.type}`);
+      console.log(`Unhandled event type ${event.type}`);
   }
 
-  //return 200 ok
-
+  // return 200 OK
   return Response.json(null, { status: 200 });
 }
